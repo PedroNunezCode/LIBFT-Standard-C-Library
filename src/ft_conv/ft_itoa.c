e1258368b355db0a4d6692651549984a4f09fe5c
PedroNunezCode <pedronunezcode@gmail.com>
2019-11-11 19:18:58 -0800

    changed the structure of my itoa

--- src/ft_conv/ft_itoa.c
+++ src/ft_conv/ft_itoa.c
@@ -11,27 +11,50 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+int		len(long nb)
+{
+	int		len;
 
-char	*ft_itoa(int n)
+	len = 0;
+	if (nb < 0)
+	{
+		nb = nb * -1;
+		len++;
+	}
+	while (nb > 0)
+	{
+		nb = nb / 10;
+		len++;
+	}
+	return (len);
+}
+
+char	*ft_itoa(int nb)
 {
 	char *str;
+	long	n;
+	int		i;
 
-	if (!(str = (char *)malloc(sizeof(char) * 2)))
+	n = nb;
+	i = len(n);
+	if (!(str = (char*)malloc(sizeof(char) * (i + 1))))
 		return (NULL);
-	if (n == -2147483648)
-		return (ft_strcpy(str, "-2147483648"));
+	str[i--] = '\0';
+	if (n == 0)
+	{
+		str[0] = 48;
+		return (str);
+	}
 	if (n < 0)
 	{
 		str[0] = '-';
-		str[1] = '\0';
-		str = ft_strjoin(str, ft_itoa(-n));
+		n = n * -1;
 	}
-	else if (n >= 10)
-		str = ft_strjoin(ft_itoa(n / 10), ft_itoa(n % 10));
-	else if (n < 10 && n >= 0)
+	while (n > 0)
 	{
-		str[0] = n + '0';
-		str[1] = '\0';
+		str[i] = 48 + (n % 10);
+		n = n / 10;
+		i--;
 	}
 	return (str);
 }
